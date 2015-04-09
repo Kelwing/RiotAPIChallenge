a365d8d4f25063c83206c15846900fb363b3459d
Kyle <kpderosh@mtu.edu>
2015-04-08 21:03:51 -0400

    Added examples of the pie charts

--- main_stats.php
+++ main_stats.php
@@ -9,6 +9,9 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="apichallenge.css" rel="stylesheet">
     <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.2.min.js"></script>
+    <link href="../css/jquery.circliful.css" rel="stylesheet" type="text/css" />
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/jquery.circliful.min.js"></script>
 </head>
 
 <body>
@@ -25,62 +28,43 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li><a href="../index.html">Recent Game Stats</a></li>
+                    <li><a href="../index.php">Recent Game Stats</a></li>
                     <li class="active"><a href="#">Nurf Game Statistics</a></li>
                 </ul>
-                <form class="navbar-form navbar-right" action="javascript:displayFilters();" method="get" role="search">
-                    <div class="form-group">
-                        <input type="text" id="summonerName" onKeyDown="if(event.keyCode==13) displayFilters();" class="form-control" placeholder="Find Stats" name="name">
-                    </div>
-                    <button type="submit"  class="btn btn-default">Submit</button>
-                </form>
-                <div id="currentGameButton" class="navbar-form navbar-right" ></div>
             </div><!--/.nav-collapse -->
         </div>
     </nav>
     <div class="header" style="margin-top:100px; text-align:center; font-size:40px">Most Popular NURF Champions</div>
     <table id="mostPopularChampions" class="table">
         <tr>
-            <th>Sion</th>
-            <th>Irelia</th>
-            <th>Sample Summoner</th>
-            <th>Hello world</th>
-            <th>Test</th>
-        </tr>
-        <tr>
             <td>
-                <div class="circle" >
-                    <div class="innerCircle">57%
-                    </div>
-                </div>
+                <div id="champ1" class="circle" data-dimension="200" data-text="" data-info="Sion" data-width="20" data-fontsize="30" data-percent="50" data-fgcolor="#61a9dc" data-bgcolor="#eee" data-fill="#ddd" data-total="100" data-part="100" data-icon="long-arrow-up" data-icon-size="28" data-icon-color="#fff"></div>
             </td>
             <td>
-                <div class="circle" >
-                    <div class="innerCircle">57%
-                    </div>
-                </div>
+                <div id="champ2" class="circle" data-dimension="200" data-text="" data-info="irelia" data-width="20" data-fontsize="30" data-percent="50" data-fgcolor="#61a9dc" data-bgcolor="#eee" data-fill="#ddd" data-total="100" data-part="60" data-icon="long-arrow-up" data-icon-size="28" data-icon-color="#fff"></div>
             </td>
             <td>
-                <div class="circle" >
-                    <div class="innerCircle">57%
-                    </div>
-                </div>
+                <div id="champ3" class="circle" data-dimension="200" data-text="" data-info="Graves" data-width="20" data-fontsize="30" data-percent="50" data-fgcolor="#61a9dc" data-bgcolor="#eee" data-fill="#ddd" data-total="100" data-part="60" data-icon="long-arrow-up" data-icon-size="28" data-icon-color="#fff"></div>
             </td>
             <td>
-                <div class="circle" >
-                    <div class="innerCircle">57%
-                    </div>
-                </div>
+                <div id="champ4" class="circle" data-dimension="200" data-text="50%" data-info="ezreal" data-width="20" data-fontsize="30" data-percent="50" data-fgcolor="#61a9dc" data-bgcolor="#eee" data-fill="#ddd" data-total="100" data-part="60" data-icon="long-arrow-up" data-icon-size="28" data-icon-color="#fff"></div>
             </td>
             <td>
-                <div class="circle" >
-                    <div class="innerCircle">57%
-                    </div>
-                </div>
+                <div id="champ5" class="circle" data-dimension="200" data-text="" data-info="bitch" data-width="20" data-fontsize="30" data-percent="50" data-fgcolor="#61a9dc" data-bgcolor="#eee" data-fill="#ddd" data-total="100" data-part="60" data-icon="long-arrow-up" data-icon-size="28" data-icon-color="#fff"></div>
             </td>
         </tr>
     </table>
-    
+    <script>
+        $( document ).ready(function() {
+            $('#champ1').circliful();
+            $('#champ1').dimension="500";
+            $('#champ2').circliful();
+
+            $('#champ3').circliful();
+            $('#champ4').circliful();
+            $('#champ5').circliful();
+        });
+    </script>
     <!--<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.2.min.js"></script>
      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
