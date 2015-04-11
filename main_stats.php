d4cd425f577f8b6af61d38b1c0e80e91721729be
Kyle Derosha <kpderosh@mtu.edu>
2015-04-10 20:45:24 -0400

    Added bar chart for highest winrate and pie charts from each rank

--- main_stats.php
+++ main_stats.php
@@ -1,3 +1,6 @@
+<?php
+require_once 'common.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,13 +10,17 @@
     
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Riot API Challenge</title>
+
     <!--JQUERY PLUGIN!-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="./js/jquery-1.11.2.min.js"></script>
+
     <!--bootstrap shit !-->
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
     <!-- custom stylesheets!-->
     <link href="apichallenge.css" rel="stylesheet">
     <link href="./barchart-plugin/main.css" rel="stylesheet">
+
     <!-- circliful pie chart shit!-->
     <link href="./circliful/css/jquery.circliful.css" rel="stylesheet" type="text/css" />
     <script src="./circliful/js/jquery.circliful.min.js"></script>
@@ -49,12 +56,20 @@
         <tr style="text-align:center">
             <td style="margin:auto">
                 <div id="winRates" data-sort="true" data-width="600" class="jChart chart-sm">
-                    <div class="define-chart-row" data-color="red" title="Pumpkin">13</div>
-                    <div class="define-chart-row" data-color="gray" title="Pecan">24</div>
-                    <div class="define-chart-row" data-color="green" title="Cherry">17</div>
-                    <div class="define-chart-row" data-color="orange" title="Apple">26</div>
-                    <div class="define-chart-row" data-color="black" title="Rhubarb">12</div>
-                    <div class="define-chart-row" data-color="blue" title="Chocolate Cream">8</div>
+		    <?php
+			$colors = array("#1000ff","#006eff","#00b6ff","#00fff6","#00ff90");
+			$winrates = getChampWinRates($stats_collection);
+			$counter = 1;
+			foreach($winrates as $stats){
+			    $winrate = $stats->winrate;
+			    echo '<div class="define-chart-row" data-color="'.$color[$counter-1].'" title="sumName">'.$winrate.'</div>';
+			    if($counter >= 5){
+			    	break;
+			    }else {
+				$counter++;
+			    }
+			}
+		    ?> 
                     <div class="define-chart-footer">10%</div>
                     <div class="define-chart-footer">20%</div>
                     <div class="define-chart-footer">30%</div>
@@ -68,28 +83,63 @@
                 </div>
             </td>
         </tr>
+	<tr>
+	    <table class="table">
+		<tr>
+    			<div class="header" style="margin-top:15px; text-align:center; font-size:40px">Division Of Ranks</div>
+		</tr>
+		<tr>
+	<?php 
+		$ranked = getRankedStats($ranked_collection);
+		$unranked = $ranked["unranked"];
+		$bronze = $ranked["bronze"];
+		$silver = $ranked["silver"];
+		$gold = $ranked["gold"];
+		$plat = $ranked["plat"];
+		$diamond = $ranked["diamond"];
+		$master = $ranked["master"];
+		$challenger = $ranked["challenger"];
+		$total = $unranked + $bronze + $silver + $gold + $plat + $diamond + $master + $challenger;
+		$unranked /= ($total * (1/100));
+		$bronze /= ($total * (1/100));
+		$silver /= ($total * (1/100));
+		$gold /= ($total * (1/100));
+		$plat /= ($total * (1/100));
+		$diamond /= ($total * (1/100));
+		$master /= ($total * (1/100));
+		$challenger /= ($total * (1/100));
+		$unranked = round($unranked,1);
+		$bronze = round($bronze,1);
+		$silver = round($silver,1);
+		$gold = round($gold,1);
+		$plat = round($plat,1);
+		$diamond = round($diamond,2);
+		$master = round($master,2);
+		$challenger = round($challenger,3);
+		$pieColors = array("#000", "#5E2323", "#757575", "#EEC900", "#38A3D8", "#fff", "#00FFA", "");
+		$rankedNames = array("Unranked", "Bronze","Silver","Gold","Platinum","Diamond","Master","Challenger");
+		$ranks = array($unranked, $bronze, $silver, $gold, $plat, $diamond, $master, $challenger);
+		$counter = 0;
+		foreach($ranks as $value){	
+			echo '<td style="margin-right:10px"><div id="rank'.$counter.'" class="circle" style="margin:auto" data-dimension="150" data-text="'.$ranks[$counter].'%" data-info="'.$rankedNames[$counter].'" data-width="20" data-fontsize="30" data-percent="35" data-fgcolor="'.$pieColors[$counter].'" data-bgcolor="#eee" data-fill="#ddd" data-total="100" data-part="'.$ranks[$counter].'" data-icon="long-arrow-up" data-icon-size="28" data-icon-color="#fff"></div></td>';
+			$counter++;
+		}
+	    ?>
+	        </tr>
+	    </table>
+	</tr>
     </table>
     
-    <div class="header" style="margin-top:100px; text-align:center; font-size:40px">Division of Ranks</div>
-    <table id="ranksTable" class="table">
-    	<tr>
-	    <th style="text-align:center">Bronze Logo</th>
-	    <th style="text-align:center">Silver Logo</th>
-	    <th style="text-align:center">Gold Logo</th>
-	    <th style="text-align:center">Platinum Logo</th>
-	    <th style="text-align:center">Diamond Logo</th>
-	</tr>
-	<tr>
-	   <!-- display the logos and percentage per league!-->
-	</tr>
-    </table>        
     <script>
         $( document ).ready(function() {
-            $('#champ1').circliful();
-            $('#champ2').circliful();
-            $('#champ3').circliful();
-            $('#champ4').circliful();
-            $('#champ5').circliful();
+            $('#rank0').circliful();
+            $('#rank1').circliful();
+            $('#rank2').circliful();
+            $('#rank3').circliful();
+            $('#rank4').circliful();
+	    $('#rank5').circliful();
+            $('#rank6').circliful();
+            $('#rank7').circliful();
             $("#winRates").jChart(); 
         });
     </script>
